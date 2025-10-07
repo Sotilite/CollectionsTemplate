bbd3ec48bf5e049bec04736dd7ba14d0afb7ea01
Alexander Govorukhin <alexandergdev20@gmail.com>
2025-10-07 15:14:30 +0500

    Второе задание готово

--- src/main/java/ru/naumen/collection/task2/Task2.java
+++ src/main/java/ru/naumen/collection/task2/Task2.java
@@ -1,6 +1,8 @@
 package ru.naumen.collection.task2;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -33,10 +35,22 @@ public class Task2
 {
 
     /**
-     * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
+     * Возвращает дубликаты пользователей, которые есть в обеих коллекциях.
+     * Выбрал HashSet, поскольку эффективно проверяет принадлежность элемента к коллекции за O(1).
+     * Алгоритмическая сложность O(n + m), где n - количество операций добавления элементов в
+     * setA за O(1) для каждой операции, m - количество операций поиска элементов из collB в
+     * HashSet за O(1) для каждой операции. Такая алгоритмическая сложность гарантируется правильным
+     * переопределением методов equals и hashCode() с использованием встроенной функции hash класса Objects.
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO реализовать метод
-        return null;
+        if(collA == null || collB == null) {
+            return  new ArrayList<>();
+        }
+
+        var setA = new HashSet<>(collA);
+
+        return collB.stream()
+                .filter(setA::contains)
+                .toList();
     }
 }
--- src/main/java/ru/naumen/collection/task2/User.java
+++ src/main/java/ru/naumen/collection/task2/User.java
@@ -1,5 +1,8 @@
 package ru.naumen.collection.task2;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Пользователь
  *
@@ -10,4 +13,22 @@ public class User {
     private String username;
     private String email;
     private byte[] passwordHash;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return Objects.equals(username, ((User) obj).username) &&
+                Objects.equals(email, ((User) obj).email) &&
+                Arrays.equals(passwordHash, ((User) obj).passwordHash);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email, Arrays.hashCode(passwordHash));
+    }
 }
